19983f36826b2b5df4cbb6e18e3758489dc9d38d
Devon Malling <devon.malling@gmail.com>
2015-05-25 01:57:55 -0600

    Done with copying from wonderous imporioum

--- models/profileSeed.js
+++ models/profileSeed.js
@@ -6,7 +6,16 @@ Profile.find({}, function(err, results){
 			name: 'Devon Malling',
 			staples: ['baked sweet potato','roasted sweet potato', 'roasted chicken'],
 			snacks: ['cricket butter', 'beef jerky'],
-			curious: ['']
+			curious: ['organ meats', 'chayote']
 		});
+		p1.save();
+
+		var p2 = new Profile({
+			name: 'Claire D.',
+			staples: ['cauliflower rice', 'avocado', 'bone broth', 'shell fish'],
+			snacks: ['kale chips', 'plantain crackers', 'coconut flour pancakes'],
+			curious: ['mayo alternatives', 'ketchup alternatives','mustard alternatives'] 
+		});
+		p2.save();
 	}
-})
+});
