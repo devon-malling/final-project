9a024425bbb856dba389b76990b3090097d3c8a8
Student <devon.malling@gmail.com>
2015-06-03 15:43:05 -0600

    small changes in seeds

--- models/profileSeed.js
+++ models/profileSeed.js
@@ -3,7 +3,7 @@ var Profile = require('./profile.js');
 Profile.find({}, function(err, results){
 	if(results.length === 0){
 		var p1 = new Profile({
-			name: 'Devon Malling',
+			name: 'Devon',
 			staples: ['baked sweet potato','roasted sweet potato', 'roasted chicken'],
 			snacks: ['cricket butter', 'beef jerky'],
 			curiousFoods: ['organ meats', 'chayote']
